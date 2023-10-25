1015b73b540247ef294cefa1d0269e171be89411
Enzo Maitan <maitanfavaroenzo@gmail.com>
2023-10-24 21:10:14 -0400

    Adds the funcionality to save a new task to a JSON

--- src/app/components/addnewtodo.tsx
+++ src/app/components/addnewtodo.tsx
@@ -1,7 +1,43 @@
-export default function AddNewTodo(){
-    return (
-        <div>
-            <textarea  placeholder="Add a new task"></textarea>
-        </div>
-    )
-  }
+"use client"
+
+import { useState } from "react";
+
+export default function AddNewTodo({ onSaveJSON } : any){
+  
+    type JsonObject = {
+        checked: boolean,
+        text: string;
+    };
+    
+    type TextAreaToJSONProps = {
+        onSaveJSON: (data: JsonObject | null) => void;
+    };
+
+    const [inputValue, setInputValue] = useState<string>("");
+    const [jsonObject, setJsonObject] = useState<JsonObject | null>(null);
+    
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (event.key === "Enter" && !event.shiftKey) {
+        event.preventDefault();
+            if (inputValue.trim()) {
+                const newJsonObject: JsonObject = {
+                    checked: false,
+                    text: inputValue.trim(),
+                };
+                setJsonObject(newJsonObject);
+                onSaveJSON(newJsonObject);
+                setInputValue("");
+            }
+        }
+    }
+        return (
+            <div>
+                <textarea 
+                        value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyUp={handleKeyPress}
+                    placeholder="Add a new task"/>
+            </div>
+        );
+    
+}
--- src/app/components/todolist.tsx
+++ src/app/components/todolist.tsx
@@ -1,27 +1,35 @@
+"use client"
+
+import { useState } from "react";
 import AddNewTodo from "./addnewtodo";
 import Progressbar from "./progressbar";
 import TodoItem from "./todoitem";
 
-export default function Todolist() {
-    const lista = {
-        "title": "TODO TODO",
-        "individual": [
-            {checked:true, text:"todo 0"},
-            {checked:false, text:"todo 1"},
-            {checked:true, text:"todo 2"}
-        ]
-    };
+var temp = {
+    "title": "",
+    "individual": []     
+};
 
+export default function Todolist() {
+    const [jsonObject, setJsonObject] = useState<any | null>(null);
+    const [lista, setLista] = useState<any | null>(temp);
+
+    const saveJSON = (data: any | null) => {
+        setJsonObject(data);
+        lista.individual.push(data);
+        console.log(lista);
+    };
+    
     return (
         <section>
         <h2>{lista.title}</h2>
         {lista.individual.map((item) => (
           <TodoItem checked={item.checked} text={item.text} onChange={false}/>
         ))}  
-        <AddNewTodo />
+        <AddNewTodo onSaveJSON={saveJSON} />
         <Progressbar    
             numberOfItems={lista.individual.length}
-            currentItems={Object.values(lista.individual).reduce((a, { checked }) => a + +checked, 0)}
+            currentItems={Object.values(lista.individual).reduce((a, { checked }) => a + +checked, 0)} 
         />
         </section>
     )
